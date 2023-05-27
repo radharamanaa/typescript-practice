4e618402847d20a5bebe52f72d40eea68561e541
Abhijeet Mishra <colouredpages@gmail.com>
2023-05-28 00:08:14 +0530

    added anagram program

--- set-practice/set.js
+++ set-practice/set.js
@@ -1,2 +1,23 @@
 "use strict";
-let set = new Set();
+// Write a function that takes two strings and returns true if
+// they are anagrams of each other, false otherwise.
+// An anagram is a word that can be formed by rearranging the letters of another word.
+function findIfAnagram(s1, s2) {
+    if (s1.length != s2.length) {
+        return false;
+    }
+    else {
+        let set = new Set();
+        for (let c of s1) {
+            set.add(c);
+        }
+        for (let c of s2) {
+            if (!set.has(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+let sol = findIfAnagram("tomato", "matoto");
+console.log(sol);
--- set-practice/set.ts
+++ set-practice/set.ts
@@ -1,1 +1,23 @@
-let set: Set<string> = new Set<string>();
+// Write a function that takes two strings and returns true if
+// they are anagrams of each other, false otherwise.
+// An anagram is a word that can be formed by rearranging the letters of another word.
+
+function findIfAnagram(s1: string, s2: string): boolean {
+  if (s1.length != s2.length) {
+    return false;
+  } else {
+    let set: Set<string> = new Set<string>();
+    for (let c of s1) {
+      set.add(c);
+    }
+    for (let c of s2) {
+      if (!set.has(c)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+let sol = findIfAnagram("tomato", "matoto");
+console.log(sol);
